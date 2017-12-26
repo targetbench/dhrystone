b95171204311092453c5f38ae4afa51fac8f0e98
hansy <hansy0419@thundersoft.com>
2017-12-26 17:02:18 +0800

    rename test file name by Sanyang

--- handlers/dhrystone_parser.py
+++ handlers/dhrystone_parser.py
@@ -1,5 +1,6 @@
 import re
-
+import json
+from caliper.server.run import parser_log
 
 def whets_parser(content, outfp):
     score = -1
@@ -22,10 +23,35 @@ def dhry2_parser(content, outfp):
         break
     return score
 
+def dhrystone(filePath, outfp):
+    cases = parser_log.parseData(filePath)
+    result = []
+    for case in cases:
+        caseDict = {}
+        caseDict[parser_log.BOTTOM] = parser_log.getBottom(case)
+        titleGroup = re.search("\[test:([\s\S]+?)\]", case)
+        if titleGroup != None:
+            caseDict[parser_log.TOP] = titleGroup.group(0)
+
+        tables = []
+        tableContent = {}
+        centerTopGroup = re.search("Dhrystone Benchmark([\s\S]+)used in the benchmark:", case)
+        tableContent[parser_log.CENTER_TOP] = centerTopGroup.group(0)
+        tableGroup = re.search("used in the benchmark:\n([\s\S]+)\n\[status\]", case)
+        if tableGroup is not None:
+            tableGroupContent = tableGroup.groups()[0].strip()
+            table = parser_log.parseTable(tableGroupContent, ":{1,}")
+            tableContent[parser_log.I_TABLE] = table
+        tables.append(tableContent)
+        caseDict[parser_log.TABLES] = tables
+        result.append(caseDict)
+    outfp.write(json.dumps(result))
+    return result
+
+
 if __name__ == "__main__":
-    infp = open("1.txt", "r")
-    content = infp.read()
-    outfp = open("2.txt", "a+")
-    whets_parser(content, outfp)
+    infile = "dhrystone_output.log"
+    outfile = "dhrystone_json.txt"
+    outfp = open(outfile, "a+")
+    dhrystone(infile, outfp)
     outfp.close()
-    infp.close()
